ccfd94837ef0189456cc53f85a1e1af2075bb7c0
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-07-04 20:53:02 +0000

    adds sql script to populate products table

---
+++ 10products.sql
@@ -0,0 +1,30 @@
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (355, "SanDisk Ultra II 1TB SATA III SSD", "Computers & Accessories", 270, 80);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (2468, "Kensun HID Xenon Conversion Kit - HB4, 6000K", "Automotive Parts & Accessories", 45, 50);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3213, "Purefly Soft Velvet Inflatable Travel Neck Pillow", "Home & Kitchen", 13, 100);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (67, "EasyAcc 20W 4A 4-Port USB Wall Charger", "Electronics", 10, 65);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (349, "Seagate 6TB IronWolf NAS SATA 6Gb/s NCQ 128MB Cache 3.5-Inch HDD", "Computers & Accessories", 190, 40);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3200, "ThermoPro TP93A Digital Food Cooking Thermometer", "Home & Kitchen", 10, 39);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (60, "Anker PowerCore 20100 - Ultra High Capacity Power Bank with 4.8A Output", "Cell Phones & Accessories", 35, 65);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (42, "Samsung Galaxy S8 64GB Unlocked Phone - US Version (Midnight Black)", "Cell Phones & Accessories", 725, 100);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (7000, "ICOCO Solar Torches Lights Dancing Flame Lights Optical Controlled Solar Flickering", "Outdoor Lighting", 32, 34);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (7023, "Newhouse Lighting Solar Flickering LED Tiki Torches, Bamboo Finish", "Outdoor Lighting", 60, 46);
