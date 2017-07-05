e36fd271b2a775b4010b2ba0562b4b01950d9ceb
Dennis Feliciano <dennis.feliciano5@gmail.com>
2017-07-05 17:37:54 +0000

    adds validation function for item_id

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,6 +26,19 @@ function openStore() {
           type: "input",
           message: "Enter the ID of the product you would like to buy",
           name: "item_id",
+          validate: function(value) {
+            var x = parseInt(value);
+            for (i = 0; i < res.length; i++) {
+
+              if (x === parseInt(res[i].item_id)) {
+                return true;
+              }
+              else if (i == (res.length - 1)) {
+              console.log("\n" + "The requested item does not exist!");
+              return false;
+              }
+            }
+          }
         },
 
         {
